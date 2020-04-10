c259fb20c420b3d987f1d8a167ebf8098f1c4bc2
bazilucmarian <48621132+bazilucmarian@users.noreply.github.com>
2020-04-10 09:47:56 -0700

    Updates

---
+++ precache-manifest.49e936b1bfb13b083f1478bab99b1344.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15b2741ece89bbd202c3bd2060d63d5",
+    "url": "/React-Redux-MOVIE-SEARCH-/index.html"
+  },
+  {
+    "revision": "1ab8fda104689aaaad1d",
+    "url": "/React-Redux-MOVIE-SEARCH-/static/css/main.d20d7e65.chunk.css"
+  },
+  {
+    "revision": "115776c63f2582c5f253",
+    "url": "/React-Redux-MOVIE-SEARCH-/static/js/2.2317a145.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-MOVIE-SEARCH-/static/js/2.2317a145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab8fda104689aaaad1d",
+    "url": "/React-Redux-MOVIE-SEARCH-/static/js/main.3bfa4d78.chunk.js"
+  },
+  {
+    "revision": "627200ddd6a16847a7e9",
+    "url": "/React-Redux-MOVIE-SEARCH-/static/js/runtime-main.15a0b536.js"
+  }
+]);
